d13d6808888deab6ce88432b951bffc1d2b41992
srisenthilnathnp <62305525+srisenthilnathnp@users.noreply.github.com>
2022-01-02 22:05:07 -0600

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,5 +1,5 @@
 public class helloworld {
    public static void main(String[] args) {
-    System.out.println("Hello World from polled by Jenkins for change");
+    System.out.println("Hello World and the build triggered by the webhook");
     }
 }
